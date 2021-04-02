a4e543e44e64bb70e5f30d57368af92e9833b09c
Manuel Vergaz Aguilera <mvergaz@yahoo.com>
2021-04-02 12:07:06 +0200

    logs folder unit test added

--- src/test/unit.test.js
+++ src/test/unit.test.js
@@ -7,6 +7,7 @@ const config = require(configFilePath)
 
 describe('service unit tests', () => {
     const port = config.find( c => c.key === "port number" ).value || 3000
+    const logsFolder = config.find( c => c.key === "logs folder" ).value
 
     test('expects the config file to exist', async () => {
         expect(fs.existsSync(configFilePath)).toBe(true)
@@ -15,4 +16,9 @@ describe('service unit tests', () => {
     test('expects port number to be > 3000', async () => {
         expect(port).toBeGreaterThan(3000)
     })
+
+    test('expects logs folder not to be null', async () => {
+        expect(logsFolder).not.toBeNull()
+    })
+
 })
